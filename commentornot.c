6c3a72f9224206ef2b6f0aa3fe534dc077529c1c
gokul704 <36412861+gokul704@users.noreply.github.com>
2021-02-05 18:36:10 +0530

    Create commentornot.c

---
+++ commentornot.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+Void main ()
+{
+char com[30];
+int i=2,a=0;
+clrscr();
+printf("\n Enter comment”);
+gets(com);
+if(com[0]=='/')
+{
+if(com[1]=='/')
+printf("\n It is a comment");
+else if(com[1]=='*')
+{
+for(i=2;i<=30;i++)
+{
+if(com[i]=='*'&&com[i+1]=='/')
+{
+printf("\n It is a comment");
+a=1;
+break;
+}
+Else
+continue;
+}
+if(a==0)printf("\n It is not a comment");
+}
+Else
+printf("\n It is not a comment");
+}
+Else
+14
+printf("\n It is not a comment");
+getch();
+}
